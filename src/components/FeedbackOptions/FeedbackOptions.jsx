41b0996ab7b0bd75ae40665d058110a2021b6f34
Тетяна Невідома <nevidomatanya89@gmail.com>
2023-04-18 21:03:59 +0300

    upd

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+import { OptionBtn } from 'components/FeedbackOptions/FeedbackOptions.styled';
+
+export function FeedbackOptions({ options, onLeaveFeedback }) {
+  return (
+    <div>
+      {options.map((option, index, options) => (
+        <OptionBtn onClick={() => onLeaveFeedback(option)} key={options[index]}>
+          {option}
+        </OptionBtn>
+      ))}
+    </div>
+  );
+}
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
